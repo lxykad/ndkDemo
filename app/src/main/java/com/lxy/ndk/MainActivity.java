1000387dcbf08de3d2447b32da2f36dd5ac6b37e
lxykad <503215875@qq.com>
2016-07-04 14:28:57 +0800

    android test

--- app/src/main/java/com/lxy/ndk/MainActivity.java
+++ app/src/main/java/com/lxy/ndk/MainActivity.java
@@ -1,13 +1,18 @@
 package com.lxy.ndk;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     static {
         System.loadLibrary("jniLib");
     }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +21,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public native String getString();
+
     public native String getData();
+
+    public native int getNum(int a, int b);
+    public native String get();
+
+    public void goSecond(View view){
+        Intent intent = new Intent(this,SecondActivity.class);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(this).toBundle());
+        }else {
+            startActivity(intent);
+        }
+    }
 
 }
--- app/src/main/jni/mainactivity.c
+++ app/src/main/jni/mainactivity.c
@@ -17,3 +17,29 @@ Java_com_lxy_ndk_MainActivity_getData(JNIEnv *env, jobject instance) {
 
     return (*env)->NewStringUTF(env, "666");
 }
+
+JNIEXPORT jint JNICALL
+Java_com_lxy_ndk_MainActivity_getNum(JNIEnv *env, jobject instance, jint a, jint b) {
+
+    // TODO
+
+
+}
+
+JNIEXPORT jstring JNICALL
+Java_com_lxy_ndk_MainActivity_get(JNIEnv *env, jobject instance) {
+
+    // TODO
+
+
+    return (*env)->NewStringUTF(env, "9999");
+}
+
+JNIEXPORT jstring JNICALL
+Java_com_lxy_ndk_SecondActivity_getTV2(JNIEnv *env, jobject instance) {
+
+    // TODO
+
+
+    return (*env)->NewStringUTF(env, "我是tv2");
+}
